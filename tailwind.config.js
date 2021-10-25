fbfe430a2dd3e43ee868c0c9716f9b765cb75044
zyq <zyq6227738@163.com>
2021-10-25 10:43:17 +0800

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    debugScreens: {
+      position: ['top', 'left']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-debug-screens')
+  ],
+}
